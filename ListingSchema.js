7a801811ca34a11c13253dcee4d1c8f1db4822ab
Blas Kojusner <bkojusner@ufl.edu>
2019-09-18 20:18:20 -0400

    Update ListingSchema.js

--- ListingSchema.js
+++ ListingSchema.js
@@ -9,7 +9,15 @@ var mongoose = require('mongoose'),
 var listingSchema = new Schema({
   /* Your code for a schema here */ 
   //Check out - https://mongoosejs.com/docs/guide.html
-
+	code: {type: String, required: true},
+	name: {type: String, required: true},
+	coordinates: {
+		latitude: Number, 
+		longitude: Number
+	},
+	address: String,
+	created_at: Date,
+	update_at: Date
 });
 
 /* Create a 'pre' function that adds the updated_at (and created_at if not already there) property 
@@ -17,6 +25,14 @@ var listingSchema = new Schema({
 */
 listingSchema.pre('save', function(next) {
   /* your code here */
+	if(!this.code || !this.name){
+		throw Error;
+	}
+	var currentDate = new Date();
+	this.update_at = currentDate;
+	if(!this.created_at)
+		this.created_at = currentDate;
+	next();
 });
 
 /* Use your schema to instantiate a Mongoose model */
